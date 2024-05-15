d35d0a0b15fa543fe3ee7c0301c68c03080ed24e
Арсений Б. <ars.bratashin@gmail.com>
2024-05-15 19:37:56 +0300

    fix search

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -1,16 +1,12 @@
+'use client'
 import Button from '@/components/Button/Button'
 import Input from '@/components/Input/Input'
 import classNames from 'classnames'
 import { useRouter } from 'next/navigation'
-import React, { FC } from 'react'
-import { useForm } from 'react-hook-form'
+import React, { FC, useState } from 'react'
 import { SearchProps } from './Search.props'
 import { cvaSearchButton, cvaSearchForm, cvaSearchInput } from './SearchStyle'
 import GlassIcon from './glass.svg'
-
-interface IFormInput {
-	search: string
-}
 
 const Search: FC<SearchProps> = ({
 	className,
@@ -19,13 +15,21 @@ const Search: FC<SearchProps> = ({
 	onChange,
 	...props
 }) => {
-	const { register, handleSubmit, formState: { errors } } = useForm<IFormInput>()
+	const [search, setSearch] = useState<string>('')
+
 	const router = useRouter()
 
-	const goToSearch = (data: IFormInput) => {
-		if (!manage) {
-			const encodedSearch = encodeURI(data.search)
+	const goToSearch = (e: React.FormEvent) => {
+		e.preventDefault()
+		if (!manage && search !== '') {
+			const encodedSearch = encodeURI(search)
 			router.push(`/search?q=${encodedSearch}`)
+		}
+	}
+
+	const handleKeyDown = (e: React.KeyboardEvent) => {
+		if (!manage && e.key == 'Enter') {
+			goToSearch(e)
 		}
 	}
 
@@ -34,21 +38,24 @@ const Search: FC<SearchProps> = ({
 			className={classNames(className, [cvaSearchForm()])}
 			{...props}
 			role='search'
-			onSubmit={handleSubmit(goToSearch)}
+			onSubmit={goToSearch}
 		>
 			<Input
 				className={cvaSearchInput()}
 				placeholder={'Поиск...'}
-				{...register('search', { required: { value: true, message: 'Поле не может быть пустым' } })}
-				error={errors.search}
+				value={manage ? value : search}
+				onChange={manage ? onChange : e => setSearch(e.target.value)}
+				onKeyDown={handleKeyDown}
 			/>
-			<Button
-				className={cvaSearchButton()}
-				appearance='primary'
-				aria-label='Искать по сайту'
-			>
-				<GlassIcon />
-			</Button>
+			{!manage && (
+				<Button
+					className={cvaSearchButton()}
+					appearance='primary'
+					aria-label='Искать по сайту'
+				>
+					<GlassIcon />
+				</Button>
+			)}
 		</form>
 	)
 }
