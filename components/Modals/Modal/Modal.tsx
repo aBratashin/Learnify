c95ea74fd30324d3cd179cc586e70070830db8c6
Арсений Б. <ars.bratashin@gmail.com>
2024-05-19 13:44:09 +0300

    fix modal

--- components/Modals/Modal/Modal.tsx
+++ components/Modals/Modal/Modal.tsx
@@ -69,7 +69,7 @@ const Modal: FC<ModalProps> = ({
 	return (
 		<>
 			<div className='justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70'>
-				<div className='relative w-full md:w-4/6 lg:w-3/6 xl:w-2/6 my-6 mx-auto h-full lg:h-auto md:h-auto'>
+				<div className='relative w-full md:w-4/6 lg:w-3/6 xl:w-2/6 mx-auto h-full lg:h-auto md:h-auto'>
 					{/* {CONTENT} */}
 					<div
 						className={`
